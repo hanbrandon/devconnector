842c1c11eb3a3fd8dc9561904c133ea0e83bfc19
Sang Hyun Han <hanbrandon@hotmail.com>
2019-11-06 05:20:44 -0800

    get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -111,4 +111,37 @@ router.post(
   }
 );
 
+// @route   Get api/profile
+// @desc    Get all Profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   Get api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+    res.json(profile);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+  } catch (error) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
