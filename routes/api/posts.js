5559fa2351d1f3e1b5b7849263ac1b4c2dcabba8
Sang Hyun Han <hanbrandon@hotmail.com>
2019-11-07 00:11:41 -0800

    Get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,4 +46,16 @@ router.post(
   }
 );
 
+// @route   GET api/posts
+// @desc    GET all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }); // -1 : most recent, 1 latest first.
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
