03a94cfbc19cbd8e2ccff1ab6c0b2d933c3f1a06
Sang Hyun Han <hanbrandon@hotmail.com>
2019-11-07 17:02:36 -0800

    register annd log in component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   // formData = It is state object that store form data
@@ -15,12 +16,12 @@ const Register = () => {
   const onChange = e =>
     setFromData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log('passwords do not match');
     } else {
-      console.log(formData);
+      console.log('SUCCESS');
     }
   };
   return (
@@ -79,7 +80,7 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
