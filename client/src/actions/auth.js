f4aa7ded5f76d1bf2779f5f4a024ed210ebe5089
Sang Hyun Han <hanbrandon@hotmail.com>
2019-11-09 23:51:01 -0800

    fixed typo:

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,29 +5,30 @@ import { setAlert } from './alert';
 import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 
 // Regiser User
-export const register = ({ name, email, password }) => async (dispatch) => {
-	const config = {
-		header: {
-			'Content-Type': 'applicatiton/json'
-		}
-	};
+export const register = ({ name, email, password }) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
 
-	const body = JSON.stringify({ name, email, password });
+  const body = JSON.stringify({ name, email, password });
+  console.log(body);
 
-	try {
-		const res = await axios.post('/api/users', body, config);
-
-		dispatch({
-			type: REGISTER_SUCCESS,
-			payload: res.data
-		});
-	} catch (error) {
-		const errors = error.response.data.errors;
-		if (errors) {
-			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-		}
-		dispatch({
-			type: REGISTER_FAIL
-		});
-	}
+  try {
+    const res = await axios.post('/api/users', body, config);
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    });
+  } catch (error) {
+    console.log(error);
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: REGISTER_FAIL
+    });
+  }
 };
