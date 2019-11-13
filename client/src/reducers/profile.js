059c9ad2171ba6d39104d1bfff8e99b26e8737a2
Sang Hyun Han <hanbrandon@hotmail.com>
2019-11-13 03:23:13 -0800

    me

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,16 @@
+const initialState = {
+    profile: null,
+    profiles: [],
+    repost: [],
+    loading: true,
+    error: {}
+}
+
+export default function(state = initialState, action ) {
+    const { type, payload } = action;
+
+    switch(type) {
+        case
+    }
+
+}
