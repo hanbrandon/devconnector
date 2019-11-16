d3287d21fe29749d49dea9fa6e082feb9b5b5887
Sang Hyun Han <hanbrandon@hotmail.com>
2019-11-16 03:53:40 -0800

    Display profiles

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE, GET_REPOS } from '../actions/types';
+import { GET_PROFILE, GET_PROFILES, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE, GET_REPOS } from '../actions/types';
 
 const initialState = {
 	profile: null,
