cd7e07892a7c7c60622265e2b03313b8b1b89fa1
Sang Hyun Han <hanbrandon@hotmail.com>
2019-11-16 04:20:57 -0800

    Fixed console bugs

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 const ProfileItem = ({ profile: { user: { _id, name, avatar }, status, company, location, skills } }) => {
 	return (
