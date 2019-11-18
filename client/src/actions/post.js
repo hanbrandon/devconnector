d35a6eab93e3221b82c0789e4a364f85522a9d99
hanbrandon <hanbrandon@hotmail.com>
2019-11-18 05:14:41 -0800

    single post display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,7 +5,8 @@ import {
 	POST_ERROR,
 	UPDATE_LIKES,
 	ADD_POST,
-	DELETE_POST
+	DELETE_POST,
+	GET_POST
 } from './types';
 
 // GET posts
@@ -107,6 +108,26 @@ export const addPost = (formData) => async (dispatch) => {
 		});
 
 		dispatch(setAlert('Post Created', 'success'));
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status
+			}
+		});
+	}
+};
+
+// GET post
+export const getPost = (id) => async (dispatch) => {
+	try {
+		const res = await axios.get(`/api/posts/${id}`);
+
+		dispatch({
+			type: GET_POST,
+			payload: res.data
+		});
 	} catch (err) {
 		dispatch({
 			type: POST_ERROR,
